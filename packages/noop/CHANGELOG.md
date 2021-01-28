a9caad81471d28b80166737166f35d8e9a4ade30
Dennis Sen <dennis.sen@dataport.de>
2021-01-28 20:22:01 +0100

    WOAH THE IMPLICATIONS

---
+++ packages/noop/CHANGELOG.md
@@ -0,0 +1,9 @@
+# CHANGELOG
+
+## 1.0.1
+
+- Nothing changed. Testing whether putting a new version in the changelog triggers a publish.
+
+## 1.0.0
+
+- Initial publish. It's feature-complete.
--- scripts/publish.js
+++ scripts/publish.js
@@ -1,14 +1,36 @@
 const cp = require('child_process')
+const fs = require('fs')
+
+function checkForNewVersion (cwd) {
+  const {version} = JSON.parse(fs.readFileSync(cwd + "/package.json", {encoding: "UTF-8"}))
+  const markdown = fs.readFileSync(cwd + "/CHANGELOG.md", {encoding: "UTF-8"})
+  const nextVersion = markdown.split("## ")[1].split("\n")[0]
+
+  if (
+    /^\d\.\d\.\d(-.+)?$/.test(nextVersion) &&
+    version !== nextVersion
+  ) {
+    return nextVersion
+  }
+}
 
 // TODO infer from glob or something
 const toPublish = ["./packages/noop"]
 
 for (const cwd of toPublish) {
   try {
-    // TODO check if package changed
-    cp.execSync('npm publish', { cwd, stdio: 'inherit' })
-    console.info(`The package ${cwd} was published.`)
+    const nextVersion = checkForNewVersion(cwd)
+    if (nextVersion) {
+      cp.execSync('npm version ' + nextVersion, { cwd, stdio: 'inherit' })
+      cp.execSync('npm ci', { cwd, stdio: 'inherit' })
+      cp.execSync('npm publish', { cwd, stdio: 'inherit' })
+      console.info(`The package ${cwd} was published.`)
+    }
+    else {
+      console.info("No update in " + cwd)
+    }
   } catch (e) {
+    console.log(e)
     /* in error case, npm already logged something - just mark procedure as failing */
     process.exitCode = 1
   }

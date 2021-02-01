2f9f517ce32bf29008f361b7bcbeafdb63d68abf
Dennis Sen <dennis.sen@dataport.de>
2021-02-01 20:25:58 +0100

    and now we check if the secret leaks

--- scripts/publish.js
+++ scripts/publish.js
@@ -5,10 +5,10 @@ const packages = require('./packages')
 function checkForNewVersion (cwd) {
   const {version} = JSON.parse(fs.readFileSync(cwd + "/package.json", {encoding: "UTF-8"}))
   const markdown = fs.readFileSync(cwd + "/CHANGELOG.md", {encoding: "UTF-8"})
-  const nextVersion = markdown.split("## ")[1].split("\n")[0]
+  const nextVersion = markdown.split("## ")[1].split("\n")[0].trim()
 
   if (
-    /^\d\.\d\.\d(-.+)?$/.test(nextVersion.trim()) &&
+    /^\d\.\d\.\d(-.+)?$/.test(nextVersion) &&
     version !== nextVersion
   ) {
     return nextVersion
@@ -19,8 +19,13 @@ for (const {path, name} of packages) {
   try {
     const nextVersion = checkForNewVersion(path)
     if (nextVersion) {
-      cp.execSync('npm version ' + nextVersion, { cwd: path, stdio: 'inherit' })
-      cp.execSync('npm publish --access=public', { cwd: path, stdio: 'inherit' })
+      const context = { cwd: path, stdio: 'inherit' }
+      cp.execSync('npm version ' + nextVersion, context)
+      cp.execSync(
+        'npm set //registry.npmjs.org/:_authToken '
+        + process.env.NODE_AUTH_TOKEN,
+      { cwd: path })
+      cp.execSync('npm publish --access=public', context)
       console.info(`The package ${name} was published.`)
     }
     else {

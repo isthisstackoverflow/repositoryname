d6aae5d6eaa331b810430e114356b3f90ff0b087
Dennis Sen <dennis.sen@dataport.de>
2021-05-04 07:59:53 +0200

    add getSetGenerator

--- packages/vuex-generators/CHANGELOG.md
+++ packages/vuex-generators/CHANGELOG.md
@@ -1,13 +1,14 @@
 # CHANGELOG
 
-## unpublished
+## 1.1.0
 
 - Added usage example to **[README](README.md)**.
 - Added directory to package.json repository link.
+- Added getSetGenerator. See **[README](README.md)**.
 
 ## 1.0.1
 
-- Added **[README](README.md)** to package. 
+- Added **[README](README.md)** to package.
 - Added GitHub repository to npm published package.
 
 ## 1.0.0
--- packages/vuex-generators/index.js
+++ packages/vuex-generators/index.js
@@ -1,4 +1,12 @@
 /**
+ * @param {string} key string to settify, e.g. 'varName'
+ * @returns {string} settified string, e.g. 'setVarName'
+ */
+function settify (key) {
+    return `set${key[0].toUpperCase()}${key.slice(1)}`
+}
+
+/**
  * Returns an object of simple mutations for a state object, where
  * simple means that they will just replace an entry for any key.
  * For example, given a state object {key: value}, an object
@@ -10,11 +18,9 @@
 export function generateSimpleMutations (state) {
     return Object.keys(state)
         .reduce((acc, key) => {
-            const mutationKey = `set${key[0].toUpperCase()}${key.slice(1)}`;
-
             return {
                 ...acc,
-                [mutationKey]: (moduleState, payload) => {
+                [settify(key)]: (moduleState, payload) => {
                     moduleState[key] = payload;
                 }
             };
@@ -36,4 +42,39 @@ export function generateSimpleGetters (state) {
             ...acc,
             [key]: s => s[key]
         }), {});
+}
+
+/**
+ * Generator to use vuex variables as v-model. Used analoguous to vuex helper functions, e.g.
+ * you may use `getSetGenerator(['varName'])` or `getSetGenerator('moduleName', ['varName'])`
+ * to create `this.varName` in vue component that may then be used as a v-model.
+ * @param {(String|String[])} prefixOrNames Either store prefix (module names '/'-separated) or ...
+ * @param {?String[]} names Array of names to generate v-model-like computed-usable property for.
+ *                          Mustn't be set if prefixOrNames is String[].
+ * @returns {Array} Array of get-settable objects
+ */
+export function getSetGenerator (prefixOrNames, names) {
+    const prefix = typeof prefixOrNames === 'string' ? `${prefixOrNames}/` : ''
+    const reducable = typeof prefixOrNames === 'string' ? names : prefixOrNames
+
+    return reducable.reduce((acc, curr) => {
+        acc[curr] = {
+            get() {
+                let pointer = this.$store.state
+                if (prefix) {
+                    prefixOrNames
+                        .split('/')
+                        .forEach((moduleName) => (pointer = pointer[moduleName]))
+                }
+                return pointer[curr]
+            },
+            set(value) {
+                this.$store.commit(
+                    `${prefix}${settify(key)}`,
+                    value
+                )
+            },
+        }
+        return acc
+    }, {})
 }
